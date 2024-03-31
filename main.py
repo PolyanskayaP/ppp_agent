5a06cdc21b24fe866118217804b8b886dd45f7c4
Полина Полянская <ppolina353@yandex.ru>
2024-03-31 19:47:33 +0300

    время

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+from datetime import datetime 
 import psutil
+
 battery = psutil.sensors_battery()
 percent = int(battery.percent)
+
 while True:
+    now = datetime.now() 
+    current_time = now.strftime("%H:%M:%S") 
     battery_2 = psutil.sensors_battery()
     percent_2 = int(battery_2.percent)
     if percent_2 != percent:
-        print(f"Заряд батареи сменился: {percent_2}%")
+        print(f"Заряд батареи сменился: {percent_2}% в {current_time}")
         battery = psutil.sensors_battery()
         percent = int(battery.percent)
